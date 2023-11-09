b63b02da335e880d1efbfd1dd19dee716d253449
Eyup Gurel <eyupgurel@gmail.com>
2023-11-09 17:17:03 +0300

    rolled back default implemented methods

--- src/sockets/binance_ob_socket.rs
+++ src/sockets/binance_ob_socket.rs
@@ -1,6 +1,7 @@
 use crate::models::common::OrderBook;
 use log::info;
 use std::net::TcpStream;
+use std::sync::mpsc::Sender;
 use serde::de::DeserializeOwned;
 use tungstenite::{connect, WebSocket};
 use tungstenite::stream::MaybeTlsStream;
@@ -19,6 +20,7 @@ impl<T> BinanceOrderBookStream<T> {
     }
 }
 
+// Implement OrderBookStream for any type T that meets the trait bounds
 impl<T> OrderBookStream<T> for BinanceOrderBookStream<T>
     where
         T: DeserializeOwned + Into<OrderBook>,
@@ -29,5 +31,54 @@ impl<T> OrderBookStream<T> for BinanceOrderBookStream<T>
 
         info!("Connected to binance stream.");
         return socket;
+    }
+
+    fn stream_ob_socket(&self, url:&str, market:&str, tx: Sender<OrderBook>, tx_diff: Sender<OrderBook>) {
+        let mut socket = self.get_ob_socket(url, market);
+        let mut last_first_ask_price: Option<f64> = None;
+        let mut last_first_bid_price: Option<f64> = None;
+
+        loop {
+            let socket_message;
+            let read = socket.read();
+
+            match read {
+                Ok(message) => {
+                    socket_message = message;
+
+                    let msg = match socket_message {
+                        tungstenite::Message::Text(s) => s,
+                        _ => {
+                            println!("Error getting text");
+                            continue;
+                        }
+                    };
+
+                    let parsed: T = serde_json::from_str(&msg).expect("Can't parse");
+                    let ob: OrderBook = parsed.into();
+
+                    let current_first_ask_price = ob.asks.first().map(|ask| ask.0.clone());
+                    let current_first_bid_price = ob.bids.first().map(|bid| bid.0.clone());
+
+                    let is_first_ask_price_changed = current_first_ask_price != last_first_ask_price;
+                    let is_first_bid_price_changed = current_first_bid_price != last_first_bid_price;
+
+                    if is_first_ask_price_changed || is_first_bid_price_changed {
+                        // Update the last known prices
+                        last_first_ask_price = current_first_ask_price;
+                        last_first_bid_price = current_first_bid_price;
+
+                        // Send the order book through the channel
+                        tx_diff.send(ob.clone()).unwrap();
+                    }
+                    tx.send(ob).unwrap();
+
+                }
+                Err(e) => {
+                    println!("Error during message handling: {:?}", e);
+                    socket =  self.get_ob_socket(url, market);
+                }
+            }
+        }
     }
 }
