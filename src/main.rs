35955069896c5908a861e006a7e7cb54179cd560
Eyup Gurel <eyupgurel@gmail.com>
2023-11-15 09:37:00 +0300

    showcase multi market mode

--- src/main.rs
+++ src/main.rs
@@ -20,18 +20,32 @@ fn main() {
     let vars: EnvVars = env::env_variables();
     env::init_logger(vars.log_level);
 
-    // start connector
-    let handle_mm = thread::spawn(move || {
 
+    // for every market we have a market maker living in a separate thread (will be moved to configuration)
+    let handle_eth_mm = thread::spawn(move || {
+        let market_map = HashMap::from([
+            ("binance", "ethusdt"),
+            ("kucoin", "ETHUSDTM"),
+            ("bluefin", "ETH-PERP"),
+        ]);
+        MM::new(market_map).connect();
+    });
+
+/*    let handle_btc_mm = thread::spawn(move || {
         let market_map = HashMap::from([
             ("binance", "btcusdt"),
             ("kucoin", "XBTUSDTM"),
             ("bluefin", "BTC-PERP"),
         ]);
         MM::new(market_map).connect();
-    });
+    });*/
 
-    handle_mm
+    handle_eth_mm
         .join()
         .expect("market maker thread failed to join main");
+
+/*    handle_btc_mm
+        .join()
+        .expect("market maker thread failed to join main");*/
+
 }
